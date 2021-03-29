72c050cadee7bdc740cae0ea9635174435ca6aee
jason <panyongpeng@ystar.io>
2021-03-29 11:20:55 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# YFVaults
+```
+93%给Vaults用户，其中45%复投、45%以挖矿对应平台的平台形式直接给用户、3%买成YF给用户（Vault里，LP池和单币池）；
+3%以USDT的形式给质押YF-USDT LP凭证的用户（boardroom里，YF矿池）；
+3%以USDT的形式留存（boardroom里，treasury）；
+1%平台自留利润
+```
---
+++ hardhat.config.js
@@ -0,0 +1,103 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    governor: {
+      default: 1,
+    },
+    custodian: {
+      default: 2,
+    },
+    boardroom: {
+      default: 3,
+    },
+    team1: {
+      default: 4,
+    },
+    team2: {
+      default: 5
+    },
+  },
+  networks: {
+    hardhat: {
+      gas: 'auto',
+      gasPrice: 'auto',
+      forking: {
+        url: 'https://http-mainnet-node.huobichain.com',
+        enabled: true
+      },
+    },
+    debug: {
+      url: 'http://127.0.0.1:8545',
+      timeout: 1200000
+    },
+    bsc: {
+      url: 'https://bsc-dataseed1.ninicoin.io'
+    },
+    heco: {
+      accounts: {
+        mnemonic: "work uphold between square hidden shield hub between sand expand hurt wide"
+      },
+      chainId: 128,
+      gas: 'auto',
+      gasPrice: 'auto',
+      url: 'https://http-mainnet-node.huobichain.com',
+    },
+    okex: {
+      url: 'https://exchaintest.okexcn.com'
+    }
+  },
+  solidity: {
+    compilers: [{
+      version: "0.6.12",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }, {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }, {
+      version: "0.5.16",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  mocha: {
+    timeout: 1200000
+  }
+};
